3f64349c6a28362705fd2284c42be63905be8241
Jorge Alexander Valencia Valencia <jorgealexandervalencia@gmail.com>
2018-03-29 16:17:11 -0500

    nuevas imagenes

--- XamUnad/XamUnad/Program.cs
+++ XamUnad/XamUnad/Program.cs
@@ -10,7 +10,7 @@
         static void Main(string[] args)
         {
             string nombre = "Alex";
-            Console.WriteLine($"Que más bien : {nombre}");
+            Console.WriteLine($"Que más bien  nuevas cosas: {nombre}");
             Console.ReadKey();
         }
     }
