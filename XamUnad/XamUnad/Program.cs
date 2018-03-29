95f492ff39d39fc90b865be68bac2c4d100de8cf
Jorge Alexander Valencia Valencia <jorgealexandervalencia@gmail.com>
2018-03-29 15:43:28 -0500

    ojo al cambio

--- XamUnad/XamUnad/Program.cs
+++ XamUnad/XamUnad/Program.cs
@@ -10,7 +10,7 @@
         static void Main(string[] args)
         {
             string nombre = "Alex";
-            Console.WriteLine($"Que más mi nombre es : {nombre}");
+            Console.WriteLine($"Que más  : {nombre}");
             Console.ReadKey();
         }
     }
