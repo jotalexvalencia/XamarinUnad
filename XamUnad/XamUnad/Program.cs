5c1ce85dd82f3370a1e6847d467f7a6f4e349847
Jorge Alexander Valencia Valencia <jorgealexandervalencia@gmail.com>
2018-03-29 15:40:40 -0500

    cambio de tam las imagenes 256x256

--- XamUnad/XamUnad/Program.cs
+++ XamUnad/XamUnad/Program.cs
@@ -10,7 +10,7 @@
         static void Main(string[] args)
         {
             string nombre = "Alex";
-            Console.WriteLine($"Hola  mi nombre es : {nombre}");
+            Console.WriteLine($"Que más mi nombre es : {nombre}");
             Console.ReadKey();
         }
     }
